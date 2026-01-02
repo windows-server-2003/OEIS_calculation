7a2bad7c93a22bd7b4b203e6f5e903af8255388a
windows-server-2003 <rin20042004@gmail.com>
2026-01-02 23:59:21 +0900

    Support up to u2048

--- contents/include/type_stepped_calculate.h
+++ contents/include/type_stepped_calculate.h
@@ -9,6 +9,8 @@ template<typename T, typename S, typename U> void type_stepped_calculate(int sta
 		U256,
 		U512,
 		U1024,
+		U1536,
+		U2048,
 	};
 	
 	std::vector<u64> dp_u64;
@@ -16,21 +18,27 @@ template<typename T, typename S, typename U> void type_stepped_calculate(int sta
 	std::vector<u256> dp_u256;
 	std::vector<u512> dp_u512;
 	std::vector<u1024> dp_u1024;
+	std::vector<u1536> dp_u1536;
+	std::vector<u1536> dp_u2048;
 	
 	init(dp_u64);
 	
 	Stage type_stage = U64;
 	for (int i = 0; i < stage; i++) {
-		if (type_stage == U64 && transit_if_needed(dp_u64, dp_u128)) type_stage = U128;
-		if (type_stage == U128 && transit_if_needed(dp_u128, dp_u256)) type_stage = U256;
-		if (type_stage == U256 && transit_if_needed(dp_u256, dp_u512)) type_stage = U512;
-		if (type_stage == U512 && transit_if_needed(dp_u512, dp_u1024)) type_stage = U1024;
+		if (type_stage == U64   && transit_if_needed(dp_u64, dp_u128))    type_stage = U128, dp_u64.clear(), dp_u64.shrink_to_fit();
+		if (type_stage == U128  && transit_if_needed(dp_u128, dp_u256))   type_stage = U256, dp_u128.clear(), dp_u128.shrink_to_fit();
+		if (type_stage == U256  && transit_if_needed(dp_u256, dp_u512))   type_stage = U512, dp_u256.clear(), dp_u256.shrink_to_fit();
+		if (type_stage == U512  && transit_if_needed(dp_u512, dp_u1024))  type_stage = U1024, dp_u512.clear(), dp_u512.shrink_to_fit();
+		if (type_stage == U1024 && transit_if_needed(dp_u1024, dp_u1536)) type_stage = U1536, dp_u1024.clear(), dp_u1024.shrink_to_fit();
+		if (type_stage == U1536 && transit_if_needed(dp_u1536, dp_u2048)) type_stage = U2048, dp_u1536.clear(), dp_u1536.shrink_to_fit();
 		
 		if (type_stage == U64)       main_func(i, dp_u64);
 		else if (type_stage == U128) main_func(i, dp_u128);
 		else if (type_stage == U256) main_func(i, dp_u256);
 		else if (type_stage == U512) main_func(i, dp_u512);
-		else                         main_func(i, dp_u1024);
+		else if (type_stage == U1024) main_func(i, dp_u1024);
+		else if (type_stage == U1536) main_func(i, dp_u1536);
+		else                          main_func(i, dp_u2048);
 	}
 }
 
--- contents/include/types.h
+++ contents/include/types.h
@@ -11,10 +11,14 @@ using u128 = boost::multiprecision::uint128_t;
 using u256 = boost::multiprecision::uint256_t;
 using u512 = boost::multiprecision::uint512_t;
 using u1024 = boost::multiprecision::uint1024_t;
+using u1536 = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<1536, 1536, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void> >;
+using u2048 = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<2048, 2048, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void> >;
 using cu128 = boost::multiprecision::checked_uint128_t;
 using cu256 = boost::multiprecision::checked_uint256_t;
 using cu512 = boost::multiprecision::checked_uint512_t;
 using cu1024 = boost::multiprecision::checked_uint1024_t;
+using cu1536 = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<1536, 1536, boost::multiprecision::unsigned_magnitude, boost::multiprecision::checked, void> >;
+using cu2048 = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<2048, 2048, boost::multiprecision::unsigned_magnitude, boost::multiprecision::checked, void> >;
 
 using s8 = int8_t;
 using s16 = int16_t;
@@ -24,10 +28,14 @@ using s128 = boost::multiprecision::int128_t;
 using s256 = boost::multiprecision::int256_t;
 using s512 = boost::multiprecision::int512_t;
 using s1024 = boost::multiprecision::int1024_t;
+using s1536 = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<1536, 1536, boost::multiprecision::signed_magnitude, boost::multiprecision::unchecked, void> >;
+using s2048 = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<2048, 2048, boost::multiprecision::signed_magnitude, boost::multiprecision::unchecked, void> >;
 using cs128 = boost::multiprecision::checked_int128_t;
 using cs256 = boost::multiprecision::checked_int256_t;
 using cs512 = boost::multiprecision::checked_int512_t;
 using cs1024 = boost::multiprecision::checked_int1024_t;
+using cs1536 = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<1536, 1536, boost::multiprecision::signed_magnitude, boost::multiprecision::checked, void> >;
+using cs2048 = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<2048, 2048, boost::multiprecision::signed_magnitude, boost::multiprecision::checked, void> >;
 
 using cpp_int = boost::multiprecision::cpp_int;
 
